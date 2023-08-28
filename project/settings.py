1c849bc5a3930d260baaaf3ada1313a4e9f786b6
ramy-ibrahim-ahmed <ramyibrahim987@gmail.com>
2023-08-28 04:50:10 +0300

    make code more readable by improve the comments

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    # configurations of rest framework
     'rest_framework',
     'tickets',
 ]
